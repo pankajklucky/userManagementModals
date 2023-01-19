447aa0ed7faf3492675249c06b38c3fd0592fd57
pankaj kumar <pankajklucky450@gmail.com>
2023-01-19 14:59:12 +0530

    initial commit

--- src/components/providers/Context.jsx
+++ src/components/providers/Context.jsx
@@ -1,7 +1,3 @@
 import { createContext } from "react";
 
-export const UserContext = createContext();
 export const AlertContext = createContext();
-export const DataContext = createContext();
-export const CartContext = createContext();
-export const ProductDetailsContext=createContext()
